067df8a91b81efb4af6aa9313969c512d3910414
Sebastian Hay <porcelainsoul@hotmail.com>
2018-03-07 08:39:08 +0000

    added click functionality to position control button

--- app/src/main/java/com/bjjskillstracker/MainScreen.java
+++ app/src/main/java/com/bjjskillstracker/MainScreen.java
@@ -1,15 +1,29 @@
 package com.bjjskillstracker;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
 import com.trainingapp.bjjskillstracker.R;
 
 public class MainScreen extends AppCompatActivity {
+
+    public ImageButton button1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_screen);
+
+        button1 = findViewById(R.id.button_PositionControl);
+        button1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v){
+            Toast.makeText(MainScreen.this, "It works", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
