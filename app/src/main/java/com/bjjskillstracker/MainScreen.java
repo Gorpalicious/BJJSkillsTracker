a69b80cbd2a80c924ea3b94737f81b6bb1381cd6
Sebastian Hay <porcelainsoul@hotmail.com>
2018-03-07 06:55:23 +0000

    added main activity screen layout

---
+++ app/src/main/java/com/bjjskillstracker/MainScreen.java
@@ -0,0 +1,15 @@
+package com.bjjskillstracker;
+
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import com.trainingapp.bjjskillstracker.R;
+
+public class MainScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_screen);
+    }
+}
